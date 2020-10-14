6a4589310e91a5c8af5b3d5f270d79ec0c33aef5
ahkam-9408 <it18098174@my.sliit.lk>
2020-10-14 09:50:57 +0530

    QR Scanner Implemented

--- app/src/main/java/com/example/inspectorapp/Home.java
+++ app/src/main/java/com/example/inspectorapp/Home.java
@@ -1,14 +1,69 @@
 package com.example.inspectorapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbarHome);
+        toolbar.setTitle("Inspector Login");
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.home_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameHome,
+                    new ScanFragment()).commit();
+            navigationView.setCheckedItem(R.id.menu_home);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_logout:
+                Toast.makeText(getApplicationContext(),"Logout...",Toast.LENGTH_LONG).show();
+                Intent mainAct = new Intent(Home.this, MainActivity.class);
+                startActivity(mainAct);
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/inspectorapp/ScanFragment.java
@@ -0,0 +1,100 @@
+package com.example.inspectorapp;
+
+import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.util.Scanner;
+
+public class ScanFragment extends Fragment {
+
+    CodeScanner codeScanner;
+    CodeScannerView scannerView;
+    TextView resultData;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_scan, container, false);
+
+        final Activity activity = getActivity();
+
+        scannerView = view.findViewById(R.id.scanner_view);
+        resultData = view.findViewById(R.id.textResult);
+
+        codeScanner = new CodeScanner(activity,scannerView);
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(activity,"Hi",Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        scannerView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view1){
+                codeScanner.startPreview();
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //codeScanner.startPreview();
+        requestForCamera();
+    }
+
+    private void requestForCamera() {
+        Dexter.withActivity(getActivity()).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(getActivity(), "Camera Permission is Required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+
+            }
+        }).check();
+    }
+
+    /*@Override
+    public void onPause() {
+        codeScanner.releaseResources();
+        super.onPause();
+    }*/
+}
